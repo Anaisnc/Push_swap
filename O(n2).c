059bdddc026e8460b8dba5ad9d38a6390e7432c0
ancourt <ancourt@z4r10p4.42lyon.fr>
2026-01-27 17:18:53 +0100

    0(n2) done ?

--- O(n2).c
+++ O(n2).c
@@ -3,10 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   O(n2).c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ancourt <ancourt@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ancourt <ancourt@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/24 19:52:45 by ancourt           #+#    #+#             */
-/*   Updated: 2026/01/24 19:52:46 by ancourt          ###   ########.fr       */
+/*   Created: 2026/01/27 17:15:24 by ancourt           #+#    #+#             */
+/*   Updated: 2026/01/27 17:15:25 by ancourt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+int *selection_sort(int *array, int size)
+{
+    int i;
+	int j;
+	int smaller;
+	int tmp;
+
+    i = 0;
+    while (i < size - 1)
+    {
+        smaller = i;
+		j = i + 1;
+        while (j < size)
+        {
+            if (array[j] < array[smaller])
+                smaller = j;
+			 j++;
+        }
+        if (smaller != i)
+        {
+            tmp = array[i];
+            array[i] = array[smaller];
+            array[smaller] = tmp;
+        }
+		i++;
+    }
+    return (array);
+}
+
+int main(void)
+{
+    int i;
+    int array[6] = {7, 5, 4, 3, 2, -1};
+
+    printf("Before: %d %d %d %d %d %d\n", array[0], array[1], array[2], array[3], array[4], array[5]);
+    selection_sort(array, 6);
+    printf("After: %d %d %d %d %d %d\n", array[0], array[1], array[2], array[3], array[4], array[5]);
+    return (0);
+}
